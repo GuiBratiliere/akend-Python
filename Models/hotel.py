3886298deb5ce3e87963f0013c1f64f68362878d
Guilherme Bratiliere <guilhermebarbosa201515@gmail.com>
2025-07-01 00:04:14 -0300

    inclusão banco de dados

--- Models/hotel.py
+++ Models/hotel.py
@@ -1,6 +1,16 @@
+from SQL_Alchemy import banco
 
 
-class  HotelModel:
+class  HotelModel(banco.Model):
+    __tablename__ = 'hoteis'
+
+    hotel_id = banco.Column(banco.String, primary_key = True)
+    nome = banco.Column(banco.String(80))
+    cidade = banco.Column(banco.String(80))
+    preco = banco.Column(banco.Float(precision=2))
+    estrelas = banco.Column(banco.Float(precision=1))
+    disponivel = banco.Column(banco.String)
+
     def __init__(self, hotel_id, nome, cidade, preco, estrelas, disponivel):
         self.hotel_id = hotel_id
         self.nome = nome
@@ -8,7 +18,7 @@ class  HotelModel:
         self.preco = preco
         self.estrelas = estrelas
         self.disponivel = disponivel
-    def jason(self):
+    def json(self):
         return{
         "id": self.hotel_id,
         "nome": self.nome,
@@ -17,3 +27,25 @@ class  HotelModel:
         "estrelas": self.estrelas,
         "disponivel": self.disponivel
         }
+    
+    @classmethod
+    def verifica_hotel(cls, hotel_id):
+        hotel = cls.query.filter_by(hotel_id=hotel_id).first()
+        if hotel:
+            return hotel
+        return None
+    
+    def save_hotel(self):
+        banco.session.add(self)
+        banco.session.commit()
+
+    def update_hotel(self, nome, cidade, preco, estrelas, disponivel):
+        self.nome = nome
+        self.cidade = cidade 
+        self.preco = preco
+        self.estrelas = estrelas
+        self.disponivel = disponivel
+
+    def delete_hotel(self):
+        banco.session.delete(self)
+        banco.session.commit()
--- app.py
+++ app.py
@@ -3,11 +3,19 @@ from flask_restful import Resource, Api
 from resources.hotel import Hoteis, Hotel
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
+
+@app.before_request
+def cria_banco():
+    banco.create_all()
 
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<int:hotel_id>')
 
 
 if __name__ == '__main__':
+    from SQL_Alchemy import banco 
+    banco.init_app(app)
     app.run(debug=True)
